ebe861b7faa490f16ee56c3014f7c8cfc5285013
Jesse Walter <TwoTinyTrees@gmail.com>
2021-12-29 15:02:39 -0600

    Through video 14

---
+++ Planner.Client.Services/Interfaces/IPlansService.cs
@@ -0,0 +1,15 @@
+﻿using Planner.Shared.Models;
+using Planner.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Client.Services.Interfaces
+{
+    public interface IPlansService
+    {
+        Task<ApiResponse<PagedList<PlanSummary>>> GetPlansAsync(string query = null, int pageNumber = 1, int pageSize = 10);
+    }
+}
